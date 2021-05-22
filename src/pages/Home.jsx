d9b55830977403f49a93f3458e14ff7b9d9fca6c
Christopher <calb200@aucklanduni.ac.nz>
2021-05-22 20:56:29 +1200

    setup flappybird and background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { startGame } from "../scripts/gamestate";
+import "./home.css";
 const Home = () => {
   return (
-    <div className="main-menu">
-      <div>Flappy Bird</div>
-      <button
-        onClick={() => {
-          startGame();
-        }}
-      >
-        Start Game
-      </button>
-    </div>
+    <>
+      <div className="game-arena">
+        <div className="main-menu">
+          <div>Flappy Bird</div>
+          <button
+            onClick={() => {
+              startGame();
+            }}
+          >
+            Start Game
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/scripts/gamestate.js
+++ src/scripts/gamestate.js
@@ -1,3 +1,12 @@
 export const startGame = () => {
-    document.getElementsByClassName("main-menu")[0].style.display = "none";
-}
+  document.getElementsByClassName("main-menu")[0].style.display = "none";
+  let flappybird = document.createElement("img");
+  flappybird.src = "./flappybird.png";
+  flappybird.classList.add("flappy-bird");
+  let gameArena = document.getElementsByClassName("game-arena")[0]
+  gameArena.appendChild(flappybird)
+};
+
+export const endGame = () => {
+  document.getElementsByClassName("main-menu")[0].style.display = "block";
+};
