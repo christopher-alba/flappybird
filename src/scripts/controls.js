f152c48d4d152f18bd4dc222783d18444d06f8c1
Christopher <calb200@aucklanduni.ac.nz>
2021-05-22 21:40:56 +1200

    debounce keyboard

--- src/scripts/controls.js
+++ src/scripts/controls.js
@@ -1,11 +1,20 @@
 import { jump } from "./animations";
 
+let spaceOn = false;
+
 export const addJumpEventListener = () => {
   document.addEventListener("keypress", onSpacebarPress);
+  document.addEventListener("keyup", onSpacebarRelease);
 };
 
 const onSpacebarPress = (event) => {
-  if (event.code === "Space") {
+  if (event.code === "Space" && spaceOn === false) {
     jump();
+    spaceOn = true;
+  }
+};
+const onSpacebarRelease = (event) => {
+  if (event.code === "Space" && spaceOn === true) {
+    spaceOn = false;
   }
 };
